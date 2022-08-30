098e3f3ce5b6e276d85eba2bc8520a145163513e
Tyler Hallada <tyler@hallada.net>
2022-08-29 23:47:15 -0400

    Add Sentry redux integration

--- lib/store.ts
+++ lib/store.ts
@@ -1,14 +1,18 @@
 import LogRocket from "logrocket"
+import * as Sentry from "@sentry/react";
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit"
 
 import pluginsReducer from "../slices/plugins"
 import pluginsTxtReducer from "../slices/pluginsTxt"
 import modListFiltersReducer from "../slices/modListFilters"
 
+const sentryReduxEnhancer = Sentry.createReduxEnhancer();
+
 export function makeStore() {
   return configureStore({
     reducer: { pluginsTxt: pluginsTxtReducer, plugins: pluginsReducer, modListFilters: modListFiltersReducer },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }).concat(LogRocket.reduxMiddleware()),
+    enhancers: [sentryReduxEnhancer],
   })
 }
 
